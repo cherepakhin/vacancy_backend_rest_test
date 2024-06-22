0535d3e1fe5fb985085a1b71f0fab48390548c1a
Черепахин Василий <vasi.che@gmail.com>
2024-06-22 16:39:45 +0500

    add CompanyRestTest.getAllCompany_checkBody()

--- src/test/kotlin/ru/perm/v/vacancy/restassured/company/CompanyRestTest.kt
+++ src/test/kotlin/ru/perm/v/vacancy/restassured/company/CompanyRestTest.kt
@@ -1,14 +1,18 @@
 package ru.perm.v.vacancy.restassured.company
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.qameta.allure.Epic
 import io.restassured.RestAssured
 import org.apache.http.HttpStatus
 import org.hamcrest.core.IsEqual.equalTo
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
 import ru.perm.v.vacancy.restassured.CONSTS
+import ru.perm.v.vacancy.restassured.dto.CompanyDTO
 
 
 @DisplayName("Company tests")
@@ -43,5 +47,24 @@ class CompanyRestTest {
             .body("n", equalTo(N))
             .and()
             .body("name", equalTo("COMPANY_1"))
+    }
+    @Test
+    @DisplayName("GET All Company. Check OK.")
+    fun getAllCompany_HttpStatusIsOK() {
+        RestAssured.given().`when`().get("/").then()
+            .statusCode(HttpStatus.SC_OK)
+            .contentType("application/json")
+    }
+    @Test
+    @DisplayName("GET All Company. Check body.")
+    fun getAllCompany_checkBody() {
+        val json = RestAssured.get("/").body.asString()
+        val companies: List<CompanyDTO>  = ObjectMapper().readValue(json)
+
+        assertEquals(4, companies.size)
+        assertEquals(CompanyDTO(-1L,"-"), companies[0])
+        assertEquals(CompanyDTO(1L,"COMPANY_1"), companies[1])
+        assertEquals(CompanyDTO(2L,"COMPANY_2"), companies[2])
+        assertEquals(CompanyDTO(3L,"3_COMPANY"), companies[3])
     }
 }
--- src/test/kotlin/ru/perm/v/vacancy/restassured/dto/CompanyDTO.kt
+++ src/test/kotlin/ru/perm/v/vacancy/restassured/dto/CompanyDTO.kt
@@ -1,4 +1,38 @@
 package ru.perm.v.vacancy.restassured.dto
 
 class CompanyDTO {
+    var n: Long = -1L
+    var name: String = ""
+
+    constructor()  {
+
+    }
+
+    constructor(n: Long, name: String) {
+        this.n = n
+        this.name = name
+    }
+
+
+    override fun toString(): String {
+        return "CompanyDTO(n=$n, name='$name')"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CompanyDTO) return false
+
+        if (n != other.n) return false
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = n.hashCode()
+        result = 31 * result + name.hashCode()
+        return result
+    }
+
+
 }
