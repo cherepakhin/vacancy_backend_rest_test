f27601b600081c5b760015e2c30b5b2961ac2e1c
Черепахин Василий <vasi.che@gmail.com>
2024-07-07 11:08:53 +0500

    val IP = "127.0.0.1:8980"

--- src/test/kotlin/ru/perm/v/vacancy/restassured/rest/CONSTS.kt
+++ src/test/kotlin/ru/perm/v/vacancy/restassured/rest/CONSTS.kt
@@ -2,7 +2,8 @@ package ru.perm.v.vacancy.restassured.rest
 
 class CONSTS {
     companion object {
-        val IP = System.getenv("VACANCY_KOTLIN_IP") ?: "127.0.0.1:8980"
+//        val IP = System.getenv("VACANCY_KOTLIN_IP") ?: "127.0.0.1:8980"
+        val IP = "127.0.0.1:8980"
         val HOST = "http://"+ IP +"/vacancy/api"
         val ECHO_PATH = HOST + "/echo/"
         val COMPANY_PATH = HOST + "/company/"
