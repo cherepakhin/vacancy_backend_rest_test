56c104b6bfd0e3af2c13fbad9689326fed73e3a5
Черепахин Василий <vasi.che@gmail.com>
2024-07-22 21:03:17 +0500

    add test with before, after

--- src/test/kotlin/ru/perm/v/vacancy/restassured/rest/CompanyRestTest.kt
+++ src/test/kotlin/ru/perm/v/vacancy/restassured/rest/CompanyRestTest.kt
@@ -3,6 +3,7 @@ package ru.perm.v.vacancy.restassured.rest
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import io.qameta.allure.Epic
+import io.restassured.RestAssured
 import io.restassured.RestAssured.*
 import org.apache.http.HttpStatus
 import org.hamcrest.core.IsEqual.equalTo
@@ -11,6 +12,7 @@ import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.slf4j.LoggerFactory
+import org.springframework.boot.web.client.RestTemplateBuilder
 
 
 @DisplayName("Company tests")
@@ -78,17 +80,34 @@ class CompanyRestTest {
     @DisplayName("GET All Company. Check body.")
     fun getAllCompany_checkBody() {
         val json = get("/").body.asString()
-        val companies: List<CompanyDTO>  = ObjectMapper().readValue(json)
+        val companies: List<CompanyDTO> = ObjectMapper().readValue(json)
 
         assertEquals(4, companies.size)
-        assertEquals(CompanyDTO(-1L,"-"), companies[0])
-        assertEquals(CompanyDTO(1L,"COMPANY_1"), companies[1])
-        assertEquals(CompanyDTO(2L,"COMPANY_2"), companies[2])
-        assertEquals(CompanyDTO(3L,"3_COMPANY"), companies[3])
+        assertEquals(CompanyDTO(-1L, "-"), companies[0])
+        assertEquals(CompanyDTO(1L, "COMPANY_1"), companies[1])
+        assertEquals(CompanyDTO(2L, "COMPANY_2"), companies[2])
+        assertEquals(CompanyDTO(3L, "3_COMPANY"), companies[3])
     }
 
+    @Test
+    @DisplayName("Create Company.")
+    fun createCompany() {
+        val N = 4L
+        //before test
+        delete("/" + N) // delete if exist
 
-    //TODO: test create
+        //test
+        val newCompany = CompanyDTO(N, "NEW_COMPANY")
+        given().body(newCompany).contentType("application/json").`when`().post("/").then().statusCode(200)
+
+        //get created company
+        given().`when`().get("/" + N).then()
+            .statusCode(HttpStatus.SC_OK)
+
+        //after test
+        delete("/" + N)
+    }
+
     //TODO: test update with exist company
     //TODO: test update with NOT exist company
     //TODO: test delete with exist company
